8403495763a04dfb18021144a762d88125fe1766
rilwan-p-r <rilwanpr@gmail.com>
2024-06-11 19:43:32 +0530

    recursionString

---
+++ DSA/stringReverseRecursion.js
@@ -0,0 +1,15 @@
+let word='helloworld'
+let result=''
+// console.log(word.slice(-1))
+// for(let i=word.length-1;i>=0;i--){
+//     result+=word[i]
+// }
+
+function reverse(str){
+    if(str.length<1){
+        return str
+    }
+    return str.slice(-1) + reverse(str.slice(0,-1))
+}
+console.log(reverse(word))
+// console.log(result)

a7f0d4a5116323548462371c67a47b1733ff8d42
rilwan-p-r <rilwanpr@gmail.com>
2024-06-12 20:55:04 +0530

    callBackHell&Promise

--- javaScript/callBackHell.js
+++ javaScript/callBackHell.js
@@ -1,5 +1,35 @@
-for(i=1; i<=5; i++){
-    setTimeOut(()=>{
-        console.log(i)
-    },i*1000)
+function firstFunction(data) {
+  return new Promise((res, rej) => {
+    res(data+1);
+  });
 }
+function secondFunction(data) {
+    return new Promise((res,rej)=>{
+        res(data+1)
+    })
+}
+function thirdFunction(data) {
+    return new Promise((res,rej)=>{
+        res(data+1);
+    })
+}
+
+// firstFunction(() => {
+//   secondFunction(() => {
+//     thirdFunction(() => {
+//       console.log("callback completed");
+//     });
+//   });
+// });
+firstFunction(1).then((res)=>{
+    return secondFunction(res)
+})
+.then((res)=>{
+    return thirdFunction(res)
+})
+.then((res)=>{
+    console.log(res)
+})
+.catch((err)=>{
+console.error(err)
+})
--- javaScript/promise.js
+++ javaScript/promise.js
@@ -1,10 +1,11 @@
 const myPromise = new Promise((resolve, reject) => {
     // Asynchronous operation
-    let success = true; // Change to false to simulate an error
+    let success = false; // Change to false to simulate an error
 
     if (success) {
         resolve('Operation successful!');
     } else {
         reject('Operation failed!');
     }
-});
+})
+myPromise.then((res)=>console.log(res)).catch((rej)=>console.log(rej))
